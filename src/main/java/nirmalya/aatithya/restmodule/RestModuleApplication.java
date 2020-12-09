bf8c48261db050e6d1fdb91dd31969e0a332ff28
jinesh142 <jineshbehera06@gmail.com>
2020-12-09 20:38:06 +0530

    Commit

--- src/main/java/nirmalya/aatithya/restmodule/RestModuleApplication.java
+++ src/main/java/nirmalya/aatithya/restmodule/RestModuleApplication.java
@@ -12,7 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 public class RestModuleApplication { 
-
+//test
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		PasswordEncoder encoder = new BCryptPasswordEncoder();
